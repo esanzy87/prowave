be8d5782dd604060035836e903de29cb955f156b
Junwon Lee <esanzy87@gmail.com>
2019-11-29 07:38:17 +0900

    .

--- auth/serializers.py
+++ auth/serializers.py
@@ -1,26 +1,16 @@
 """
 auth.serializers
 """
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from prowave.models import UserInfo
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-    """
-    사용자 정보
-    """
-    class Meta:
-        model = UserInfo
-        fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.Serializer):
     """
     사용자 (인증)
     """
-    profile = ProfileSerializer()
-
-    class Meta:
-        model = User
-        fields = '__all__'
+    user_id = serializers.IntegerField(source='id')
+    name = serializers.CharField(source='first_name')
+    email = serializers.EmailField()
+    country = serializers.CharField(source='info.country')
+    organization = serializers.CharField(source='info.organization')
+    title = serializers.CharField(source='info.title')
--- auth/viewsets.py
+++ auth/viewsets.py
@@ -1,9 +1,13 @@
 """
 auth.viewsets
 """
-from rest_framework import viewsets, routers
+from django.contrib.auth.models import User
+from django.db import transaction, IntegrityError
+from rest_framework import viewsets, routers, status
+from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from prowave.models import UserInfo
 from .serializers import UserSerializer
 
@@ -13,21 +17,83 @@ class UserViewSet(viewsets.ModelViewSet):
     회원
     """
     serializer_class = UserSerializer
-    queryset = UserInfo.objects.all()
+    queryset = User.objects.all()
     permission_classes = (AllowAny,)
 
     def get_queryset(self):
+        """
+        UserViewSet.get_queryset
+        """
         if self.request.user.is_authenticated:
             if self.request.user.is_superuser:
                 return self.queryset
             return self.queryset.filter(user=self.request.user)
         raise PermissionDenied
 
+    @transaction.atomic()
+    def create(self, request, *args, **kwargs):
+        """
+        UserViewSet.create
+        """
+        try:
+            create_user_args = {
+                'username': request.data['email'],
+                'email': request.data['email'],
+                'password': request.data['password'],
+                'first_name': request.data['name'],
+            }
+
+            instance = User.objects.create_user(**create_user_args)
+
+            create_user_info_args = {
+                'country': request.data['country'],
+                'organization': request.data['organization'],
+                'title': request.data['title'],
+                'user': instance
+            }
+
+            UserInfo.objects.create(**create_user_info_args)
+        except KeyError as error:
+            return Response({
+                'success': False,
+                'detail': 'Required field %s is not provided.' % error
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except IntegrityError:
+            return Response({
+                'success': False,
+                'detail': 'The email you have entered (%s) already exists.' % request.data['email']
+            }, status=status.HTTP_403_FORBIDDEN)
+        except:
+            return Response({
+                'success': False,
+                'detail': 'Sorry, We have failed to register your information. We will try to fix this issue as soon as possible.'
+            })
+
+        return Response({
+            'success': True,
+            'created_user': self.serializer_class(instance),
+        }, status=status.HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        raise PermissionDenied(detail="This API is not supported.")
+
     def retrieve(self, request, *args, **kwargs):
-        user_info = self.get_object()
-        if request.user.is_authenticated and user_info.user.id == request.user.id:
+        """
+        UserViewSet.retrieve
+        """
+        instance = self.get_object()
+        if request.user.is_authenticated and instance.id == request.user.id:
             return super().retrieve(request, *args, **kwargs)
         raise PermissionDenied
+
+    @action(detail=False, methods=['get'])
+    def whoami(self, request):
+        """
+        UserViewSet.whoami
+        """
+        if not request.user.is_authenticated:
+            raise PermissionDenied
+        return Response(self.serializer_class(request.user).data)
 
 
 router = routers.DefaultRouter()
